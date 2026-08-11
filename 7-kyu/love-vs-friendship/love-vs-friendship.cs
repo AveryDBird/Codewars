ebccb9c32c829bbf3bbe94b4502f35e52b63e399
Avery Bird <156035502+AveryDBird@users.noreply.github.com>
2026-08-11 10:06:23 -0500

    Add solution - CodeHub

---
+++ 7-kyu/love-vs-friendship/love-vs-friendship.cs
@@ -0,0 +1,17 @@
+using System;
+​
+public static class Kata
+{
+  public static int WordsToMarks(string str)
+  {
+  int intValue =0;
+    foreach (char c in str)
+      {
+        intValue += c-96;
+      }
+    
+    
+    return intValue;
+   // throw new NotImplementedException();
+  }
+}
